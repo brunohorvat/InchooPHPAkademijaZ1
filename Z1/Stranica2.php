95391be08b28839fa128fd52f629f1407afad410
Bruno Horvat <46749189+brunohorvat@users.noreply.github.com>
2019-01-17 12:50:41 +0100

    Add files via upload

---
+++ Z1/Stranica2.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en" xml:lang="en">
+<head>
+    <title>Stranica 1</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/style.css">
+</head>
+<body>
+<p> Menu:</p>
+
+
+
+<form action="/Z1/index.html">
+    <input type="submit"class="btn btn-primary" value="Stranica1" />
+</form>
+
+<form action="/Z1/Stranica2.php">
+    <input type="submit"class="btn btn-primary" value="Stranica2" />
+</form>
+
+<form action="/Z1/Stranica3.html">
+    <input type="submit"class="btn btn-primary" value="Stranica3" />
+</form>
+
+<form action="/Z1/Stranica4.html">
+    <input type="submit"class="btn btn-primary" value="Stranica4" />
+</form>
+
+<form action="/Z1/Stranica5.html">
+    <input type="submit"class="btn btn-primary" value="Stranica5" />
+</form>
+
+
+
+</body>
+</html>
